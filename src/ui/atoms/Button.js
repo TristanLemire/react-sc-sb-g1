b4b689b5a19163bf2f5a9ac65b6dade45bf54eba
TristanLemire <tristan.lemire.sio@gmail.com>
2020-05-22 17:11:15 +0200

    Add Disabled button

--- src/ui/atoms/Button.js
+++ src/ui/atoms/Button.js
@@ -13,13 +13,18 @@ export const colors = {
     brand_primary: Colors.brand_primary,
     green: Colors.green,
     light_green: Colors.light_green,
-    grey: Colors.neutral_grey,
+    darker_grey: Colors.darker_grey,
     white: Colors.white
 };
 
 const ButtonWrapper = styled.button`
-    background: ${props => (props.color)};
-    color: ${props => (props.color === Colors.white || props.color === Colors.light_green ? Colors.black : Colors.white)}; 
+    &:not([disabled]) {
+        background: ${props => (props.color)};
+        color: ${props => (props.color === Colors.white || props.color === Colors.light_green ? Colors.black : Colors.white)};
+        cursor: pointer;
+    } 
+    background-color: ${Colors.darker_grey};
+    color: ${Colors.disabled_grey};
     padding: ${props => (props.type === type.icon ? "14px" : "8px" )} ${props => (props.type === type.primary ? "16px" : props.type === type.secondary ? "64px" : "30px")} ;
     border-radius: ${props => (props.type === type.icon ? "38px" : "4px")}; 
     font-weight: 600;
@@ -27,26 +32,34 @@ const ButtonWrapper = styled.button`
     line-height: 140%;
     font-family: sans-serif;
     border: none;
+    cursor: not-allowed;
+
+    &:hover{
+        &:not([disabled]) {
+            opacity: 0.75;
+        }
+    }
 `;
 
 const TwitchButton = props => {
-    const { text, type, color} = props;
+    const { text, type, color, disabled} = props;
     const IconLeft = props.iconLeft
     const IconRight = props.iconRight
 
-    if (text && !IconLeft && !IconRight) return <ButtonWrapper color={color} type={type}>{text}</ButtonWrapper>;
+    if (text && !IconLeft && !IconRight) return <ButtonWrapper disabled={disabled} color={color} type={type}>{text}</ButtonWrapper>;
 
-    else if (IconLeft && !text && !IconRight) return <ButtonWrapper color={color} type={type}><IconLeft/></ButtonWrapper>;
+    else if (IconLeft && !text && !IconRight) return <ButtonWrapper disabled={disabled} color={color} type={type}><IconLeft/></ButtonWrapper>;
 
-else if (IconLeft && text && !IconRight) return <ButtonWrapper color={color} type={type}><IconLeft/>{text}</ButtonWrapper>;
+    else if (IconLeft && text && !IconRight) return <ButtonWrapper disabled={disabled} color={color} type={type}><IconLeft/>{text}</ButtonWrapper>;
 
-    else if (IconLeft && text && IconRight) return <ButtonWrapper color={color} type={type}><IconLeft/>{text}<IconRight/></ButtonWrapper>;
+    else if (IconLeft && text && IconRight) return <ButtonWrapper disabled={disabled} color={color} type={type}><IconLeft/>{text}<IconRight/></ButtonWrapper>;
 };
 
 TwitchButton.propTypes = {
     type: PropTypes.string,
     text: PropTypes.string,
-    colors: PropTypes.string
+    colors: PropTypes.string,
+    disabled: PropTypes.bool
 };
 
 TwitchButton.defaultProps = {
@@ -54,7 +67,8 @@ TwitchButton.defaultProps = {
     text: "",
     iconLeft: "",
     iconRight: "",
-    colors: Colors.brand_primary
+    colors: Colors.brand_primary,
+    disabled: false
 };
 
 export default TwitchButton;
--- src/ui/particles/Colors.js
+++ src/ui/particles/Colors.js
@@ -2,6 +2,7 @@ const Colors = {
     brand_primary: "#9146FF",
     brand_light: "#AF85CD",
     neutral_grey: "#9F9F9F",
+    disabled_grey: "#aaaaaa",
     green: "#54BC58",
     light_green: "#00e6cb",
     white: "#fff",
