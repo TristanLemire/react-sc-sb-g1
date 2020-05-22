5d678cc7137fefe1f43b247400a4d8eaaf44e59a
ETombuyses <e.tombuyses@gmail.com>
2020-05-22 17:44:07 +0200

    icon

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Badge, { colors } from "./ui/atoms/Badge";
 import TwitchButton from './ui/atoms/Button'
-
-// icon button : 
-import { FaBeer } from 'react-icons/fa';
+import Icon from './ui/atoms/Icon';
 
 
 export default function App() {
-      return (
-            <div className="App">
-                <Badge text="IRL" color={colors.purple}/>
-                <Badge text="IRL" color={colors.grey}/>
-                <TwitchButton text="hello" type="primary" iconLeft={FaBeer}></TwitchButton>
-            </div>
-      );
+  return (
+    <div className="App">
+      <Badge text="IRL" color={colors.purple}/>
+      <Badge text="IRL" color={colors.grey}/>
+      <TwitchButton text="hello" type="primary" color="#9146FF" iconLeft="star"></TwitchButton>
+      <Icon color="pink"></Icon>
+    </div>
+  );
 }
--- src/ui/atoms/Button.js
+++ src/ui/atoms/Button.js
@@ -1,58 +1,59 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 import Colors from "../particles/Colors";
+import Icon from "../atoms/Icon"
 
 export const type = {
-    primary: "primary",
-    secondary: "secondary",
-    icon: "icon",
+  primary: "primary",
+  secondary: "secondary",
+  icon: "icon",
 };
 
 export const colors = {
-    brand_primary: Colors.brand_primary,
-    green: Colors.green,
-    grey: Colors.grey,
-    white: Colors.white
+  brand_primary: Colors.brand_primary,
+  green: Colors.green,
+  grey: Colors.grey,
+  white: Colors.white
 };
 
 const ButtonWrapper = styled.button`
-    background: ${props => (props.color)};
-    color: ${props => (props.type === type.secondary ? Colors.black : Colors.white)}; 
-    padding: ${props => (props.type === type.icon ? "14px" : "8px" )} ${props => (props.type === type.primary ? "16px" : props.type === type.secondary ? "64px" : "30px")} ;
-    border-radius: ${props => (props.type === type.icon ? "38px" : "4px")}; 
-    font-weight: 600;
-    font-size: 12px;
-    line-height: 140%;
-    font-family: sans-serif;
+  background: ${props => (props.color)};
+  color: ${props => (props.type === type.secondary ? Colors.black : Colors.white)}; 
+  padding: ${props => (props.type === type.icon ? "14px" : "8px" )} ${props => (props.type === type.primary ? "16px" : props.type === type.secondary ? "64px" : "30px")} ;
+  border-radius: ${props => (props.type === type.icon ? "38px" : "4px")}; 
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 140%;
+  font-family: sans-serif;
 `;
 
 const TwitchButton = props => {
-    const { text, type, color} = props;
-    const IconLeft = props.iconLeft
-    const IconRight = props.iconRight
+  const { text, type, color, iconLeft, iconRight} = props;
 
-    if (text && !IconLeft && !IconRight) return <ButtonWrapper color={color} type={type}>{text}</ButtonWrapper>;
+return <ButtonWrapper color={color} type={type}>
+  {iconLeft && <Icon></Icon>}
+  {text}
+  {iconRight && <Icon></Icon>}
 
-    else if (IconLeft && !text && !IconRight) return <ButtonWrapper color={color} type={type}><IconLeft/></ButtonWrapper>;
+  </ButtonWrapper>;
 
-else if (IconLeft && text && !IconRight) return <ButtonWrapper color={color} type={type}><IconLeft/>{text}</ButtonWrapper>;
-
-    else if (IconLeft && text && IconRight) return <ButtonWrapper color={color} type={type}><IconLeft/>{text}<IconRight/></ButtonWrapper>;
 };
 
 TwitchButton.propTypes = {
-    type: PropTypes.string,
-    text: PropTypes.string,
-    colors: PropTypes.string
+  type: PropTypes.string,
+  text: PropTypes.string,
+  colors: PropTypes.string,
+  iconLeft: PropTypes.string,
+  iconRight: PropTypes.string
 };
 
 TwitchButton.defaultProps = {
-    type: type.primary,
-    text: "",
-    iconLeft: "",
-    iconRight: "",
-    colors: Colors.brand_primary
+  type: type.primary,
+  text: "",
+  iconLeft: "",
+  iconRight: "",
+  colors: Colors.brand_primary
 };
 
 export default TwitchButton;
