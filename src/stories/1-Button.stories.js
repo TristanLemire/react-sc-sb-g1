f2dfcfb41c8eb07bd83e47e7aa198a896c5b8e33
TristanLemire <tristan.lemire.sio@gmail.com>
2020-05-22 15:35:00 +0200

    add figma addon

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -9,6 +9,15 @@ export default {
 
 export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
 
+Text.story = {
+  parameters: {
+     design: {
+        type: 'figma',
+        url: 'https://www.figma.com/file/S2kl75dqM33oYg3tzqzShp/Module-Design-System?node-id=7%3A718'
+     }
+  }
+}
+
 export const Emoji = () => (
   <Button onClick={action('clicked')}>
     <span role="img" aria-label="so cool">
